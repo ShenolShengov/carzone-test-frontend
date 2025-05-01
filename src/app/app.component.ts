322ef080747eb77b6a974e513690d46911406725
Shenol Shengov <shenolshengov41@gmail.com>
2025-05-01 14:59:50 +0300

    Set up Prime Icons and add some examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { PrimeIcons, MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule],
+  imports: [ButtonModule, MenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'carzone-test-frontend';
+  items!: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'New',
+                icon: PrimeIcons.PLUS,
+            },
+            {
+                label: 'Delete',
+                icon: PrimeIcons.TRASH
+            }
+        ];
+    }
 }
